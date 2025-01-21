99ac93a3e8704c19db631453d4d7dfe64a3709fa
farawaystar <md.sh1@protonmail.com>
2025-01-21 13:52:06 +0100

    added StakerRegistry

--- programs/cdp_stablecoin_protocol/src/state/stake.rs
+++ programs/cdp_stablecoin_protocol/src/state/stake.rs
@@ -9,3 +9,24 @@ pub struct StakeAccount {
     pub last_staked: i64,
     pub bump: u8,
 }
+
+#[account]
+pub struct StakerRegistry {
+    pub stake_accounts: Vec<StakeAccount>,
+    // pub total_stake_points: u64,
+}
+
+
+impl StakerRegistry {
+    pub fn update_stake_account(&mut self, stake_account: StakeAccount) {
+        if let Some(index) = self.stake_accounts.iter().position(|x| x.user == stake_account.user) {
+            self.stake_accounts[index] = stake_account;
+        } else {
+            self.stake_accounts.push(stake_account);
+        }
+    }
+
+    pub fn remove_stake_account(&mut self, user: Pubkey) {
+        self.stake_accounts.retain(|x| x.user != user);
+    }
+}
